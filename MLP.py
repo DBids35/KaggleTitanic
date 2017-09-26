ce1a21315a2f01b83e5b75de2c65b45bc6f8b653
Drew Bidlen <drew.bidlen@yahoo.com>
2017-09-26 08:40:16 -0400

    Delete MLP.py

--- MLP.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-data=pd.read_csv('Data/train.csv')
-data=data.drop(['Name', 'Ticket','Cabin', 'Embarked'],1)
-sexDict={'male':0, 'female':1}
-data['Sex']=data['Sex'].apply(lambda x: sexDict[x])
-data['Age'].fillna(data['Age'].mean(), inplace=True)
-print(data.head(10))
